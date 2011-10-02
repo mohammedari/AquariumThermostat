1da75b58be985ca7c73c0651312b2bc5a3286c91
mohammedari <mohammedarixx@gmail.com>
2011-10-02 16:47:00 +0900

    EEPROM書きこみ時にページ境界をまたぐ書き込みが可能に

--- TankController/eeprom.hpp
+++ TankController/eeprom.hpp
@@ -2,6 +2,7 @@
 
 #include "i2c.hpp"
 #include "wait.hpp"
+#include "stddef.h"
 
 namespace tank_controller
 {
@@ -10,6 +11,8 @@ class eeprom
 {
     private:
     static const unsigned char _address = 0xA0; //1010 0000
+    static const size_t _page_size = 32;
+    static const unsigned char _write_cycle_ms = 5;
     util::i2c& _i2c;
 
     public:
@@ -18,14 +21,7 @@ class eeprom
     template<class T>
     void save(unsigned int address, const T& t)
     {
-        while(_i2c.is_busy());
-        
-        _i2c.start(_address & 0xFE);    //書き込みモード
-        _i2c.write((const unsigned char*)&address, sizeof(unsigned int));
-        _i2c.write((const unsigned char*)&t, sizeof(T));
-        _i2c.stop();
-    
-        wait(5);    //書き込み待ち
+        write(address, (const unsigned char*)&t, sizeof(T));
     }
     
     template<class T>
@@ -42,6 +38,35 @@ class eeprom
         _i2c.stop();
     
         return reinterpret_cast<T&>(t);
+    }
+    
+    private:
+
+    bool check_page_overflow(unsigned int address, size_t size)
+    {
+        //先頭アドレスと末端アドレスが同じページにあるかどうかチェック
+        return address / _page_size == ( address + size - 1 ) / _page_size;
+    }
+    
+    void write(unsigned int address, const unsigned char* data, size_t size)
+    {
+        if(check_page_overflow(address, size))  //ページ境界チェック
+        {
+            while(_i2c.is_busy());
+        
+            _i2c.start(_address & 0xFE);    //書き込みモード
+            _i2c.write((const unsigned char*)&address, sizeof(unsigned int));
+            _i2c.write(data, size);
+            _i2c.stop();
+    
+            util::wait(_write_cycle_ms);    //書き込み待ち
+        }
+        else
+        {
+            size_t s = _page_size - address % _page_size;   //先頭アドレスのページに書き込める大きさ
+            write(address, data, s);
+            write(address + s + 1, data + s + 1, size - s);
+        }
     }
 };
     
